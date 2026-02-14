15e4cff1fecc447470319340de53dc62fcc9ede9
Ranjithkumar Pandiyan <82885190+Ranjithkumar-Pandiyan@users.noreply.github.com>
2026-02-14 10:59:49 +0000

    Add security group and update EC2 instance configuration
    Added a security group for web access and configured ingress and egress rules. Updated EC2 instance to use the new security group.

--- main.tf
+++ main.tf
@@ -21,3 +21,36 @@ resource "aws_instance" "my_server" {
     Name = "Terraform_Automated_EC2"
   }
 }
+resource "aws_security_group" "web_sg" {
+  name = "web-security-group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "my_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "Terraform-Docker-Server"
+  }
+}
